28f4834056dd97338f5d9a3cf97c296aeb215b3e
Brodski <cbrodski@gmail.com>
2020-04-23 02:03:21 -0600

    service in prog

---
+++ app/src/main/java/com/example/mediaplayer/AudioPlayerService.kt
@@ -0,0 +1,199 @@
+package com.example.mediaplayer
+
+import android.app.Notification
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
+import android.os.Bundle
+import android.os.IBinder
+import android.support.v4.media.MediaDescriptionCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.annotation.DrawableRes
+import androidx.annotation.MainThread
+import androidx.annotation.Nullable
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.ext.mediasession.TimelineQueueNavigator
+
+class AudioPlayerService : Service() {
+
+
+    private var episodeTitle: String? = null
+    private var exoPlayer: SimpleExoPlayer? = null
+    private var playerNotificationManager: PlayerNotificationManager? = null
+    private var mediaSession: MediaSessionCompat? = null
+    private var mediaSessionConnector: MediaSessionConnector? = null
+
+    private val PLAYBACK_CHANNEL_ID = "playback_channel"
+    private val PLAYBACK_NOTIFICATION_ID = 1
+
+    private val ARG_URI = "uri_string"
+    private val ARG_TITLE = "title"
+    private val ARG_START_POSITION = "start_position"
+    private val MEDIA_SESSION_TAG = "hello-world-media"
+
+    override fun onCreate() {
+        super.onCreate()
+        val context: Context = this
+        exoPlayer = SimpleExoPlayer.Builder(this).build()
+
+        //https://medium.com/google-exoplayer/easy-audio-focus-with-exoplayer-a2dcbbe4640e
+//        val audioAttributes = AudioAttributes.Builder()
+//            .setUsage(C.USAGE_MEDIA)
+//            .setContentType(C.CONTENT_TYPE_MOVIE)
+//            .build()
+//        exoPlayer!!.setAudioAttributes(audioAttributes, true)
+
+        val audioUri = Uri.parse("https://storage.googleapis.com/exoplayer-test-media-0/Jazz_In_Paris.mp3")
+        val mp4VideoUri: Uri = Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+        val dataSourceFactory: DataSource.Factory =
+            DefaultDataSourceFactory(this, Util.getUserAgent(this, this.getString(R.string.app_name)) )
+
+        var concatenatingMediaSource: ConcatenatingMediaSource = ConcatenatingMediaSource()
+
+        val ms: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(audioUri)
+        val ms2: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(mp4VideoUri)
+        concatenatingMediaSource.addMediaSource(ms)
+        concatenatingMediaSource.addMediaSource(ms2)
+
+        exoPlayer!!.prepare(concatenatingMediaSource)
+        exoPlayer!!.playWhenReady = true
+
+        // Setup notification and media session.
+        playerNotificationManager = PlayerNotificationManager.createWithNotificationChannel(
+            context,
+            PLAYBACK_CHANNEL_ID,
+            R.string.playback_channel_name, //local name in settings dialog for the user
+            PLAYBACK_NOTIFICATION_ID,
+            object : PlayerNotificationManager.MediaDescriptionAdapter {
+                override fun getCurrentContentTitle(player: Player): String {
+                    //return player.currentWindowIndex.toString() + "title"
+                    return "title"
+                }
+
+                @Nullable
+                override fun createCurrentContentIntent(player: Player): PendingIntent? = PendingIntent.getActivity(
+                    applicationContext,
+                    0,
+                    Intent(applicationContext, MainActivity::class.java),
+                    PendingIntent.FLAG_UPDATE_CURRENT)
+
+
+                @Nullable
+                override fun getCurrentContentText(player: Player): String? {
+                    // return  player.currentWindowIndex.toString() + "text/desciption"
+                    return  "text/desciption"
+                }
+
+                @Nullable
+                override fun getCurrentLargeIcon(player: Player, callback: PlayerNotificationManager.BitmapCallback): Bitmap? {
+                    //return getBitmapFromVectorDrawable(applicationContext, R.drawable.ic_queue_music)
+                    return getBitmapFromVectorDrawable(context, R.drawable.ic_queue_music)
+                }
+            },
+            object : PlayerNotificationManager.NotificationListener {
+//                override fun onNotificationStarted(notificationId: Int, notification: Notification) {
+//                    startForeground(notificationId, notification)
+//                }
+                override fun onNotificationCancelled(notificationId: Int) {
+                  //  _playerStatusLiveData.value = PlayerStatus.Cancelled(episodeId)
+                    stopSelf()
+                }
+                override fun onNotificationPosted(notificationId: Int, notification: Notification, ongoing: Boolean) {
+                    if (ongoing) {
+                        // Make sure the service will not get destroyed while playing media.
+                        startForeground(notificationId, notification)
+                    } else {
+                        // Make notification cancellable.
+                        stopForeground(false)
+                    }
+                }
+            }
+        )
+        playerNotificationManager!!.setPlayer(exoPlayer)
+
+
+        mediaSession = MediaSessionCompat(context, MEDIA_SESSION_TAG)
+        mediaSession!!.isActive = true
+        playerNotificationManager!!.setMediaSessionToken(mediaSession!!.sessionToken)
+        mediaSessionConnector = MediaSessionConnector(mediaSession!!)
+        mediaSessionConnector?.setQueueNavigator(object : TimelineQueueNavigator(mediaSession!!) {
+                override fun getMediaDescription(player: Player, windowIndex: Int ): MediaDescriptionCompat {
+                    return mediaHelper(context, null)
+                }
+            })
+        mediaSessionConnector!!.setPlayer(exoPlayer)
+
+//        //playerNotificationManager.setPlayer(exoPlayer)
+//        playerNotificationManager?.setMediaSessionToken(mediaSession!!.sessionToken)
+
+    }
+
+    private fun mediaHelper(context: Context, someshit: Unit?): MediaDescriptionCompat {
+        var extras: Bundle? = null
+        var bitmap: Bitmap? = getBitmapFromVectorDrawable(context, R.drawable.ic_queue_music)
+        extras?.putParcelable(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, bitmap)
+        extras?.putParcelable(MediaMetadataCompat.METADATA_KEY_DISPLAY_ICON, bitmap)
+
+        return MediaDescriptionCompat.Builder()
+            .setMediaId("123")
+            .setIconBitmap(bitmap)
+            .setTitle("DANK TITLE!")
+            .setDescription("REAL DESCRIP")
+            .setExtras(extras)
+            .build()
+
+    }
+
+    @MainThread
+    private fun getBitmapFromVectorDrawable(context: Context, @DrawableRes drawableId: Int): Bitmap? {
+        return ContextCompat.getDrawable(context, drawableId)?.let {
+            val drawable = DrawableCompat.wrap(it).mutate()
+
+            val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+
+            bitmap
+        }
+    }
+
+    override fun onDestroy() {
+        mediaSession?.release()
+        mediaSessionConnector?.setPlayer(null)
+        playerNotificationManager?.setPlayer(null)
+        exoPlayer!!.release()
+        exoPlayer = null
+
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        ////To change body of created functions use File | Settings | File Templates.
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        //return super.onStartCommand(intent, flags, startId)
+        return START_STICKY
+    }
+}
--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.example.mediaplayer
 
+import android.app.Notification
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.media.audiofx.AudioEffect
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.DrawableRes
+import androidx.annotation.MainThread
+import androidx.annotation.Nullable
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ServiceCompat.stopForeground
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.source.SampleStream
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
@@ -18,6 +38,9 @@ class MainActivity : AppCompatActivity() {
 
     private var playerView: PlayerView? = null
     private var player: SimpleExoPlayer? = null
+    private var playerNotificationManager: PlayerNotificationManager? = null
+    private val CHANNEL_ID = "69 channel"
+    private val NOTIFICATION_ID = 420
     private var playWhenReady = true
     private var currentWindow = 0
     private var playBackPosition: Long = 0
@@ -41,9 +64,8 @@ class MainActivity : AppCompatActivity() {
     private fun buildMediaSource(uri: Uri): MediaSource {
         val dataSourceFactory: DataSource.Factory =
             DefaultDataSourceFactory(this, Util.getUserAgent(this, this.getString(R.string.app_name)) )
-        val videoSource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
-        Log.e(TAG, "BUILD " + this.getString(R.string.app_name))
 
+        val videoSource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
         val audioUri = Uri.parse("https://storage.googleapis.com/exoplayer-test-media-0/Jazz_In_Paris.mp3")
 
         val mediaSource2: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(audioUri)
@@ -55,6 +77,7 @@ class MainActivity : AppCompatActivity() {
     private fun releasePlayer() {
         if (player != null) {
             playWhenReady = player!!.playWhenReady
+            Log.e("release,when ready", playWhenReady.toString())
             playBackPosition = player!!.currentPosition
             currentWindow = player!!.currentWindowIndex
             player!!.release()
@@ -64,12 +87,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var intent: Intent = Intent(this, AudioPlayerService::class.java)
+
+        Util.startForegroundService(this, intent)
     }
 
     override fun onStart() {
         super.onStart()
         if (Util.SDK_INT >= Build.VERSION_CODES.N) {
-            initializePlayer()
+        //    initializePlayer()
         }
     }
 
@@ -77,21 +103,21 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         //  hideSystemUi();
         if (Util.SDK_INT < Build.VERSION_CODES.N || player == null) {
-            initializePlayer()
+       //     initializePlayer()
         }
     }
 
     override fun onPause() {
         super.onPause()
         if (Util.SDK_INT < Build.VERSION_CODES.N) {
-            releasePlayer()
+     //       releasePlayer()
         }
     }
 
     override fun onStop() {
         super.onStop()
         if (Util.SDK_INT >= Build.VERSION_CODES.N) {
-            releasePlayer()
+      //      releasePlayer()
         }
     }
 }
